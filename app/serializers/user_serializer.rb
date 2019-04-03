a925aaa55f3962ee64b8d344b78572ee2b49f023
github username <github email address>
2019-04-03 11:03:28 -0400

    added ids to 'my' stuff for user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,50 @@
 
 class UserSerializer < ActiveModel::Serializer
 
-  attributes :id, :username
-  has_many :user_genres
-  has_many :user_artists
-  has_many :user_albums
-  has_many :user_songs
+  attributes :id, :username, :my_songs, :my_artists, :my_albums, :my_genres
   has_many :playlists
+
+  def my_songs
+    object.user_songs.map do |i|
+      @song = Song.find(i.song_id)
+      @album = Album.find(@song.album_id)
+      {
+        id: @song.id,
+        name: @song.name,
+        album: @album.name
+      }
+    end
+  end
+
+  def my_artists
+    object.user_artists.map do |i|
+      @artist = Artist.find(i.artist_id)
+      {
+        id: @artist.id,
+        name: @artist.name
+      }
+    end
+  end
+
+  def my_albums
+    object.user_albums.map do |i|
+      @album = Album.find(i.album_id)
+      {
+        id: @album.id,
+        name: @album.name,
+        release_year: @album.release_year
+      }
+    end
+  end
+
+  def my_genres
+    object.user_genres.map do |i|
+      @genre = Genre.find(i.genre_id)
+      {
+        id: @genre.id,
+        name: @genre.name
+      }
+    end
+  end
 
 end
