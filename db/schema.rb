a8804fb4029dd20f808623a2dc45248c39107264
github username <github email address>
2019-04-06 14:35:32 -0400

    added password to user, added bcrypt/has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2019_04_01_195540) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
