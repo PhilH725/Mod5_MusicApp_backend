2a4faf575767f82372a50322faeffcb338be0b63
github username <github email address>
2019-04-08 21:12:58 -0400

    made create playlist_songs function return json equivalent to that of a displayed playlist for optimistic rendering

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -2,9 +2,15 @@
 class PlaylistSongsController < ApplicationController
 
   def create
+    songData = []
     params[:songs].each do |i|
-      PlaylistSong.create(playlist_id: params[:playlist_id], song_id: i[:id])
+      song = Song.find(i[:id])
+      PlaylistSong.create(playlist_id: params[:playlist_id], song_id: song.id)
+      i = {id: song.id, name: song.name, artist: song.artist.name, album: song.album.name}
+      songData << i
     end
+    render json: songData
+
   end
 
 end
