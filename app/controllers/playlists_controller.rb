171fbc1abc746a072b129d0bb005f919d7fc4b1f
github username <github email address>
2019-04-27 20:29:26 -0400

    added functionality for updating songs on a playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -23,7 +23,23 @@ class PlaylistsController < ApplicationController
 
   def update
     @playlist = Playlist.find(params[:id])
+
+    old_ids = @playlist.songs.map{|s| s.id}
+    new_ids = params[:playlist][:songs].map{|i| i[:id]}
+    songs_to_add = new_ids.select{|i| !old_ids.include?(i)}
+    songs_to_remove = old_ids.select{|i| !new_ids.include?(i)}
+
+    for i in PlaylistSong.where(song_id: songs_to_remove, playlist_id: 22)
+      i.destroy
+    end
+
+    for i in songs_to_add
+      @new_song = PlaylistSong.new(playlist_id: @playlist.id, song_id: i)
+      @new_song.save
+    end
+
     @playlist.update(playlist_params)
+
     render json: @playlist
   end
 
